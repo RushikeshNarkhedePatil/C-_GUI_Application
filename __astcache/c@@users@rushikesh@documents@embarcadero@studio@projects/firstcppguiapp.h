3fc1a43e05a3d43e4951adfc8efc6c8dc4f37485
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2023-05-07 15:46:28 +0530

    first commit

---
+++ __astcache/c@@users@rushikesh@documents@embarcadero@studio@projects/firstcppguiapp.h
@@ -0,0 +1,37 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef FirstCppGUIAppH
+#define FirstCppGUIAppH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.ListView.Adapters.Base.hpp>
+#include <FMX.ListView.Appearances.hpp>
+#include <FMX.ListView.hpp>
+#include <FMX.ListView.Types.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+#include <FMX.Layouts.hpp>
+#include <FMX.ListBox.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *Label1;
+	TButton *Button1;
+	TEdit *Edit1;
+	TListBox *ListBox1;
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall ListBox1ItemClick(TCustomListBox * const Sender, TListBoxItem * const Item);
+
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
